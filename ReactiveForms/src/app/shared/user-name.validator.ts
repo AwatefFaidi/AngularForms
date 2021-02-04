2716dfdc9d49a9add66f002b4869a1f57db4035f
AwatefFaidi <feidiawatef@gmail.com>
2021-02-04 13:35:45 +0400

    Angular-forms

---
+++ ReactiveForms/src/app/shared/user-name.validator.ts
@@ -0,0 +1,15 @@
+import { ValidatorFn, AbstractControl} from '@angular/forms';
+/*
+export function forbiddenNameValidator(control: AbstractControl): { [key: string]: any } | null
+{
+    const forbidden=/admin/.test(control.value);
+    return forbidden ? { 'forbiddenName': {value:control.value}}:null;
+}*/
+// create validator ffunction with params: replcae /admin/ with forbiddenName
+export function forbiddenNameValidator(forbiddenName: RegExp): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const forbidden = forbiddenName.test(control.value);
+      return forbidden ? { 'forbiddenName': { value: control.value } } : null;
+    };
+  }
+  
